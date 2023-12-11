752a509b176a8ee10f7a691e42c98b55c2cf7341
evnotevan <74320083+evnotevan@users.noreply.github.com>
2023-12-10 21:23:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-// Function to keep track of the current day and time
 function trackDateTime() {
     const currentDate = new Date();
     console.log(`Current Date and Time: ${currentDate}`);
 }
 
-// Function to send an alert using JavaScript
 function showAlert() {
     alert('This is an alert!');
 }
 
-// Function to use a Math method
 function useMathMethod() {
-    const randomNumber = Math.random();
+    const randomNumber = Math.floor(Math.random() * 100);
     console.log(`Random Number: ${randomNumber}`);
 }
 
-// Function to delay one command or run a command at a specified interval
 function delayedCommand() {
     console.log('This command is delayed.');
 }
 
-// Function to convert between a number and a text
 function convertNumberToText() {
     const number = 42;
     const text = number.toString();
     console.log(`Number: ${number}, Text: ${text}`);
 }
 
-// Function for a simple countdown
 function countdown(seconds) {
     let currentSeconds = seconds;
-
     const intervalId = setInterval(() => {
         console.log(`${currentSeconds} seconds remaining`);
-
         if (currentSeconds === 0) {
             console.log('Countdown complete!');
             clearInterval(intervalId);
@@ -43,10 +35,22 @@ function countdown(seconds) {
     }, 1000);
 }
 
-// Call the functions
-trackDateTime();
-showAlert();
-useMathMethod();
-setTimeout(delayedCommand, 2000); // Delayed command after 2 seconds
-convertNumberToText();
-countdown(5); // Countdown for 5 seconds
+// Call the functions after the page has loaded
+window.onload = function () {
+    trackDateTime();
+    showAlert();
+    useMathMethod();
+    setTimeout(delayedCommand, 2000); // Delayed command after 2 seconds
+    convertNumberToText();
+    countdown(5); // Countdown for 5 seconds
+
+    // Update the time every second
+    setInterval(function() {
+        const currentTime = new Date();
+        const hours = currentTime.getHours().toString().padStart(2, '0');
+        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+        const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+        document.getElementById('current-time').textContent = `${hours}:${minutes}:${seconds}`;
+    }, 1000);
+};
+
